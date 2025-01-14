ac016792dcbbb47c75924b23ce413a0f3e7b88ca
Jervie Manabat <jervie.manabat@student.ateneo.edu>
2025-01-14 15:37:50 +0800

    Update Bush.java

--- Bush.java
+++ Bush.java
@@ -1,20 +1,9 @@
 /**
  Bush class implements the DrawingObject interface and creates an array of circles of random positions and colors to represent a bush.
  It also has a "draw" method to draw the bush using the "Graphics2D" object.
- @author Jervie S. Manabat (223961)
+ @author Jervie S. Manabat 
  @version March 6,2023
  **/
-/*
-I have not discussed the Java language code in my program
-with anyone other than my instructor or the teaching assistants
-assigned to this course.
-I have not used Java language code obtained from another student,
-or any other unauthorized source, either modified or unmodified.
-If any Java language code or documentation used in my program
-was obtained from another source, such as a textbook or website,
-that has been clearly noted with a proper citation in the comments
-of my program.
-*/
 
 import java.awt.Graphics2D;
 
