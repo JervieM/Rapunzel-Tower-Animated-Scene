ec2ac91c9c8a59482537abbee2bac43124d70733
Jervie Manabat <jervie.manabat@student.ateneo.edu>
2025-01-14 15:41:14 +0800

    Update SceneStarter.java

--- SceneStarter.java
+++ SceneStarter.java
@@ -1,20 +1,10 @@
 /**
  SceneStarter class contains the main method, which
  initializes a SceneFrame object and calls its setUpGUI method,
- @author Jervie S. Manabat (223961)
+ @author Jervie S. Manabat 
  @version March 6,2023
  **/
-/*
-I have not discussed the Java language code in my program
-with anyone other than my instructor or the teaching assistants
-assigned to this course.
-I have not used Java language code obtained from another student,
-or any other unauthorized source, either modified or unmodified.
-If any Java language code or documentation used in my program
-was obtained from another source, such as a textbook or website,
-that has been clearly noted with a proper citation in the comments
-of my program.
-*/
+
 public class SceneStarter {
     public static void main(String[] args) {
     // Create a new SceneFrame object
