03c38ae06e04dbeb54c4124b499bc7da85327b27
Jervie Manabat <jervie.manabat@student.ateneo.edu>
2025-01-14 15:41:01 +0800

    Update SceneFrame.java

--- SceneFrame.java
+++ SceneFrame.java
@@ -3,20 +3,9 @@
  graphical user interface (GUI) of the scene. It sets up the frame properties, such as its
  size and title, and adds the SceneCanvas component to it. It also creates and adds the JMenuBar
  to the frame, which contains the "File" and "Help" menus. It also displays the frame to the user.
- @author Jervie S. Manabat (223961)
+ @author Jervie S. Manabat 
  @version March 6,2023
  **/
-/*
-I have not discussed the Java language code in my program
-with anyone other than my instructor or the teaching assistants
-assigned to this course.
-I have not used Java language code obtained from another student,
-or any other unauthorized source, either modified or unmodified.
-If any Java language code or documentation used in my program
-was obtained from another source, such as a textbook or website,
-that has been clearly noted with a proper citation in the comments
-of my program.
-*/
 
 import javax.swing.*;
 import java.awt.*;
