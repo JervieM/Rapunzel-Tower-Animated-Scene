e2a54ab6747bdf3093bf59be18a10b2ea24570cc
Jervie Manabat <jervie.manabat@student.ateneo.edu>
2025-01-14 15:38:59 +0800

    Update GradientBackground.java

--- GradientBackground.java
+++ GradientBackground.java
@@ -1,20 +1,10 @@
 /**
  GradientBackground class is a Swing component that paints a gradient background from a top color to a bottom color.
  The top and bottom colors are fixed, and the size of the component determines the size of the gradient.
- @author Jervie S. Manabat (223961)
+ @author Jervie S. Manabat 
  @version March 6,2023
  **/
 /*
-I have not discussed the Java language code in my program
-with anyone other than my instructor or the teaching assistants
-assigned to this course.
-I have not used Java language code obtained from another student,
-or any other unauthorized source, either modified or unmodified.
-If any Java language code or documentation used in my program
-was obtained from another source, such as a textbook or website,
-that has been clearly noted with a proper citation in the comments
-of my program.
-*/
 
 import java.awt.Color;
 import java.awt.GradientPaint;
