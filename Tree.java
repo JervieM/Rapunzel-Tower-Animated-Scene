94258b43c9b63bc9132ee9f94443298d1dd6acf4
Jervie Manabat <jervie.manabat@student.ateneo.edu>
2025-01-14 15:42:54 +0800

    Update Tree.java

--- Tree.java
+++ Tree.java
@@ -2,20 +2,9 @@
  Tree class implements DrawingObject interface and represents a tree made up of a rectangle and a triangle of different colors.
  It has a constructor that takes the position, size, and color values as input parameters, and it overrides the draw method of the
  DrawingObject interface to draw the tree using the Graphics2D object passed as an argument.
- @author Jervie S. Manabat (223961)
+ @author Jervie S. Manabat 
  @version March 6,2023
  **/
-/*
-I have not discussed the Java language code in my program
-with anyone other than my instructor or the teaching assistants
-assigned to this course.
-I have not used Java language code obtained from another student,
-or any other unauthorized source, either modified or unmodified.
-If any Java language code or documentation used in my program
-was obtained from another source, such as a textbook or website,
-that has been clearly noted with a proper citation in the comments
-of my program.
-*/
 
 import java.awt.*;
 
