7b2b1a65f23b9a810e7966682018c1ce3a20798f
Jervie Manabat <jervie.manabat@student.ateneo.edu>
2025-01-14 15:38:27 +0800

    Update Cloud.java

--- Cloud.java
+++ Cloud.java
@@ -3,20 +3,10 @@
  The cloud can be moved to the right, and if it goes off the right side of the canvas, it will wrap around
  to the left side. The cloud is created with a given color and position.
  This class implements DrawingObject.
- @author Jervie S. Manabat (223961)
+ @author Jervie S. Manabat 
  @version March 6,2023
  **/
-/*
-I have not discussed the Java language code in my program
-with anyone other than my instructor or the teaching assistants
-assigned to this course.
-I have not used Java language code obtained from another student,
-or any other unauthorized source, either modified or unmodified.
-If any Java language code or documentation used in my program
-was obtained from another source, such as a textbook or website,
-that has been clearly noted with a proper citation in the comments
-of my program.
-*/
+
 import java.awt.*;
 
 public class Cloud implements DrawingObject {
