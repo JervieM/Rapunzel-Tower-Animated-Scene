642f1930e7f50f992c45c3125afbce7ca4bdac4e
Jervie Manabat <jervie.manabat@student.ateneo.edu>
2025-01-14 15:41:50 +0800

    Update SquareWithSemiCircleTop.java

--- SquareWithSemiCircleTop.java
+++ SquareWithSemiCircleTop.java
@@ -2,20 +2,9 @@
  SquareWithSemiCircleTop class is a subclass of the Square class that represents a filled square with a semicircle on top.
  It has a constructor that takes the position, size, and color values as input parameters, and it overrides the draw method
  of the Square class to draw a semicircle on top of the square using the Circle class, which is another DrawingObject implementation.
- @author Jervie S. Manabat (223961)
+ @author Jervie S. Manabat 
  @version March 6,2023
  **/
-/*
-I have not discussed the Java language code in my program
-with anyone other than my instructor or the teaching assistants
-assigned to this course.
-I have not used Java language code obtained from another student,
-or any other unauthorized source, either modified or unmodified.
-If any Java language code or documentation used in my program
-was obtained from another source, such as a textbook or website,
-that has been clearly noted with a proper citation in the comments
-of my program.
-*/
 
 import java.awt.Graphics2D;
 
